d8376cb64499e8f2269af4c4f12f6d96aee54533
mmoiozo <ozo1994@live.nl>
2015-02-23 18:31:38 +0100

    updat 2

--- modules/BottomCamSearch/bottomcamsearch.c
+++ modules/BottomCamSearch/bottomcamsearch.c
@@ -15,7 +15,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Paparazzi; see the file COPYING.  If not, write to
- * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * the Free Software Foundationc, 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  */
 
